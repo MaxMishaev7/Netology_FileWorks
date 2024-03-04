468432c840293117abbaaa6aded32954b8e75b20
MaxMishaev <maxmishaev74@gmail.com>
2024-03-04 21:00:52 +0300

    The file Main.java has been completely changed. New version of program.

--- src/Main.java
+++ src/Main.java
@@ -3,99 +3,67 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        String rootPath = "C://Game/";
+        String rootPath = "C://Games/";
         String partOfPath = "";
         String fileName = "";
+        StringBuilder strTemp = new StringBuilder();
+        String tempStr = "";
+        java.time.LocalDateTime dateTime;
         String[][] dirArr = {
                 {"src", "src/main", "src/test"}, //"src/main/Main.java"
                 {"res", "res/drawables", "res/vectors", "res/icons"},
                 {"savegames"},
                 {"temp"}
         };
-        String[][] fileArr = {
-                {"src/main", "Main.java", "Utils.java"},
-                {"temp", "temp.txt"}
-        };
-
         for(String[] paths : dirArr){
             for(String path : paths){
                 partOfPath = path;
                 java.io.File dir = new java.io.File(rootPath + partOfPath);
                 if(dir.mkdir()){
-                    System.out.println("The Folder was created");
+                    tempStr = "The Folder " + dir.getName() + " with path " + dir.getAbsolutePath() + " created";
+
+                    System.out.println("The Folder " + dir.getName() + " with path " + dir.getAbsolutePath() + " created");
                 } else {
-                    System.out.println("The Folder already exists");
+                    tempStr = "The Folder " + dir.getName() + " already exists";
+                    System.out.println("The Folder " + dir.getName() + " already exists");
                 }
+                 dateTime = java.time.LocalDateTime.now();
+                strTemp = strTemp.append(dateTime).append(" ").append(tempStr).append(System.lineSeparator());
             }
         }
-        String filePath = "";
-        for(String[] paths : fileArr){
-            for(int i = 0; i < paths.length; i++){
-                if(i == 0){
-                    filePath = paths[i];
-                    System.out.println(filePath);
-                } else {
-                    fileName = paths[i];
+        String[] fileArr = {"src/main/Main.java", "src/main/Utils.java", "temp/temp.txt"};
+        for(String pathFile : fileArr)
+        {
+            java.io.File file = new java.io.File(rootPath + "/" + pathFile);
+            try
+            {
+                if(file.createNewFile())
+                {
+                    tempStr = "File " + file.getName() + " with path " + file.getAbsolutePath() + " has been created";
+                    System.out.println("File " + file.getName() + " with path " + file.getAbsolutePath() + " has been created");
                 }
-                java.io.File file = new java.io.File(rootPath + filePath, fileName);
-                try{
-                    if(file.createNewFile()){
-                        System.out.println("File was created");
-                    }else{
-                        System.out.println("Perhaps file already exists");
-                    }
-                    fileName = "";
-                }catch(java.io.IOException ex){
-                    ex.getMessage();
+                else
+                {
+                    tempStr = "File " + file.getName() + " already exists";
+                    System.out.println("File " + file.getName() + " already exists");
                 }
+                dateTime = java.time.LocalDateTime.now();
+                strTemp = strTemp.append(dateTime).append(" ").append(tempStr).append(System.lineSeparator());
+            }
+            catch(java.io.IOException ex)
+            {
+                ex.getMessage();
             }
         }
 
-//        for (String[] paths : pathArr) {
-//            if (paths.length > 1) {
-//                for (String path : paths) {
-//                    if (path.indexOf('.') != -1) {
-//                        int slashIndex = path.lastIndexOf("/");
-//                        fileName = path.substring(slashIndex + 1);
-//                        System.out.println(fileName);
-//                        fullPath = path.substring(0, slashIndex + 1);
-//                        System.out.println(fullPath);
-//                        //java.io.File dir = new java.io.File(rootPath + fullPath, fileName);
-//                        fullPath = rootPath + fullPath + "/" + fileName;
-//                    } else {
-//                        fullPath = path;
-//                        System.out.println(fullPath);
-//                        //java.io.File dir = new java.io.File(rootPath + fullPath);
-//                        fullPath = rootPath + path;
-//                    }
-//                    java.io.File dir = new java.io.File(fullPath);
-//                    Main.createFoldersAndFiles(dir);
-//                }
-//            } else {
-//                for (String path : paths) {
-//                    fullPath = path;
-//                    System.out.println(fullPath);
-//                    java.io.File dir = new java.io.File(rootPath + fullPath);
-//                }
-//            }
-            //System.out.println();
-
-
+        try(java.io.FileWriter fwr = new java.io.FileWriter("C://Games/temp/temp.txt", false))
+        {
+           fwr.write(strTemp.toString());
+        }
+        catch(java.io.IOException ex)
+        {
+            ex.getMessage();
+        }
     }
 
-//    public static void createFoldersAndFiles(java.io.File fileOrFolder) {
-//        if (fileOrFolder.isDirectory()) {
-//            if (fileOrFolder.mkdir()) {
-//                System.out.println("Папка " + fileOrFolder.getPath() + "создана");
-//            }
-//        } else if (fileOrFolder.isFile()) {
-//            try {
-//                if (fileOrFolder.createNewFile()) {
-//                    System.out.println("Файл" + fileOrFolder.getPath() + fileOrFolder.getName() + "cоздан");
-//                }
-//            } catch (java.io.IOException ex) {
-//                System.out.println(ex.getMessage());
-//            }
-//        }
-//    }
 }
